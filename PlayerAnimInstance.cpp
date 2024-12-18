798156c57309e0cbf7dc50e11a04a649f182ccfa
Carl Jones <161535663+TripleHitcombo@users.noreply.github.com>
2024-12-18 12:57:23 +0000

    Update PlayerAnimInstance.cpp
    Updated code

--- PlayerAnimInstance.cpp
+++ PlayerAnimInstance.cpp
@@ -3,7 +3,7 @@
 
 #include "Animations/PlayerAnimInstance.h"
 
-void UPlayerAnimInstance::UpdateVelocity()
+void UPlayerAnimInstance::UpdateSpeed()
 {
 	APawn* PawnRef{ TryGetPawnOwner() };
 
@@ -14,5 +14,31 @@ void UPlayerAnimInstance::UpdateVelocity()
 
 	FVector Velocity{ PawnRef->GetVelocity() };
 
-	CurrentVelocity = static_cast<float>(Velocity.Length());	
+	CurrentSpeed = static_cast<float>(Velocity.Length());	
+}
+
+void UPlayerAnimInstance::HandleUnpdatedTarget(AActor* NewTargetActorRef)
+{
+	bIsInCombat = IsValid(NewTargetActorRef);
+}
+
+void UPlayerAnimInstance::UpdateDirection()
+{
+	APawn* PawnRef{ TryGetPawnOwner() };
+
+	if (!IsValid(PawnRef))
+	{
+		return;
+	}
+	if (!bIsInCombat)
+	{
+		return;
+	}
+
+	CurrentDirection = CalculateDirection
+	(
+	 PawnRef->GetVelocity(), 
+	 PawnRef->GetActorRotation()
+	);
+	
 }
