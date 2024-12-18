ce0f69b665b6a5e86a6ba2e0a419f5e629b1b141
Carl Jones <161535663+TripleHitcombo@users.noreply.github.com>
2024-12-18 12:59:32 +0000

    Update TraceComponent.h
    Updated code

--- TraceComponent.h
+++ TraceComponent.h
@@ -12,9 +12,31 @@ class HERO_SOULS_API UTraceComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
+	USkeletalMeshComponent* SkeletalComp;
+
+	UPROPERTY(EditAnywhere)
+	FName Start;
+
+	UPROPERTY(EditAnywhere)
+	FName End;
+
+	UPROPERTY(EditAnywhere)
+	FName Rotation;
+
+	UPROPERTY(EditAnywhere)
+	double BoxCollisionLength{ 30.0 };
+
+	UPROPERTY(EditAnywhere)
+	bool bDebugMode{ false };
+
+	TArray<AActor*> TargetsToIgnore;
+
 public:	
 	// Sets default values for this component's properties
 	UTraceComponent();
+
+	UPROPERTY(VisibleAnywhere)
+	bool bIsAttacking { false }; 
 
 protected:
 	// Called when the game starts
@@ -24,5 +46,7 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+	UFUNCTION (BlueprintCallable)
+	void HandleResetAttack();
+
 };
