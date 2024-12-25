a2124791f3e747f42ed2ebd281b7cef5f9bbf099
Carl Jones <161535663+TripleHitcombo@users.noreply.github.com>
2024-12-25 13:32:56 +0000

    Update PlayerActionsComponent.h
    Updated Code

--- PlayerActionsComponent.h
+++ PlayerActionsComponent.h
@@ -6,15 +6,41 @@
 #include "Components/ActorComponent.h"
 #include "PlayerActionsComponent.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_SPARSE_DELEGATE_OneParam
+(
+	FOnSprintSignature,
+	UPlayerActionsComponent,
+	OnSprintDelegate,
+	float, Cost
+);
+
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class HERO_SOULS_API UPlayerActionsComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
+	ACharacter* CharacterRef;
+
+	class IMainCharacterInterface* IMainCharacter;
+
+	class UCharacterMovementComponent* MovementComp;
+
+	UPROPERTY(EditAnywhere)
+	float SprintCost{ 0.1f };
+
+	UPROPERTY(EditAnywhere)
+	float SprintSpeed{ 1000.0f };
+
+	UPROPERTY(EditAnywhere)
+	float WalkSpeed{ 500.0f };
+
 public:	
 	// Sets default values for this component's properties
 	UPlayerActionsComponent();
+
+	UPROPERTY(BlueprintAssignable)
+	FOnSprintSignature OnSprintDelegate;
 
 protected:
 	// Called when the game starts
@@ -24,5 +50,10 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+	UFUNCTION(BlueprintCallable)
+	void Sprint();
+
+	UFUNCTION(BlueprintCallable)
+	void Walk();
+
 };
