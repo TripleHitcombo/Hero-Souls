6992c8b15e80660af4193a3afd8a486d501f7a30
Carl Jones <161535663+TripleHitcombo@users.noreply.github.com>
2024-12-25 13:33:26 +0000

    Update StatsComponent.h
    Updated Code

--- StatsComponent.h
+++ StatsComponent.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "Characters/EStat.h"
 #include "StatsComponent.generated.h"
 
 
@@ -12,9 +13,22 @@ class HERO_SOULS_API UStatsComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
+	UPROPERTY(EditAnywhere)
+	double StaminaRegenRate{ 10.0 };
+
+	UPROPERTY(VisibleAnywhere)
+	bool bCanRegen{ true };
+
+	UPROPERTY(EditAnywhere)
+	float StaminaDelayDuration{ 2.0f };
+
+
 public:	
 	// Sets default values for this component's properties
 	UStatsComponent();
+
+	UPROPERTY (EditAnywhere)
+	TMap<TEnumAsByte<EStat>, float> Stats;
 
 protected:
 	// Called when the game starts
@@ -23,6 +37,18 @@ protected:
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	UFUNCTION(BlueprintCallable)
+	void ReduceHealth(float Amount);
+
+	UFUNCTION(BlueprintCallable) 
+	void ReduceStamina(float Amount);
+
+	UFUNCTION(BlueprintCallable)
+	void RegenStamina();
+
+	UFUNCTION()
+	void EnableRegen();
 
 		
 };
