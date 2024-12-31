1b997c3c69f06d87e696f45df9acf772bf974e0f
Carl Jones <161535663+TripleHitcombo@users.noreply.github.com>
2024-12-31 14:15:08 +0000

    Update StatsComponent.h
    Updated Code

--- StatsComponent.h
+++ StatsComponent.h
@@ -7,6 +7,29 @@
 #include "Characters/EStat.h"
 #include "StatsComponent.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_SPARSE_DELEGATE_OneParam
+(
+	FOnHealthPercentUpdateSignature,
+	UStatsComponent, OnHealthPercentUpdateDelegate,
+	float, Percentage
+
+);
+
+DECLARE_DYNAMIC_MULTICAST_SPARSE_DELEGATE_OneParam
+(
+	FOnStaminaPercentUpdateSignature,
+	UStatsComponent, OnStaminaPercentUpdateDelegate,
+	float, Percentage
+
+);
+
+DECLARE_DYNAMIC_MULTICAST_SPARSE_DELEGATE
+(
+	FOnZeroHealthSignature,
+	UStatsComponent, OnZeroHealthDelegate
+
+
+	);
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class HERO_SOULS_API UStatsComponent : public UActorComponent
@@ -30,6 +53,15 @@ public:
 	UPROPERTY (EditAnywhere)
 	TMap<TEnumAsByte<EStat>, float> Stats;
 
+	UPROPERTY(BlueprintAssignable)
+	FOnHealthPercentUpdateSignature OnHealthPercentUpdateDelegate;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnStaminaPercentUpdateSignature OnStaminaPercentUpdateDelegate;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnZeroHealthSignature OnZeroHealthDelegate;
+
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
@@ -39,7 +71,7 @@ public:
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 	UFUNCTION(BlueprintCallable)
-	void ReduceHealth(float Amount);
+	void ReduceHealth(float Amount, AActor* Opponent);
 
 	UFUNCTION(BlueprintCallable) 
 	void ReduceStamina(float Amount);
@@ -50,5 +82,7 @@ public:
 	UFUNCTION()
 	void EnableRegen();
 
+	UFUNCTION(BlueprintPure) 
+	float GetStatPercentage(EStat Current, EStat Max);
 		
 };
