505b9ca6bff2d562ffc2f358254a7c49348cb2e2
Carl Jones <161535663+TripleHitcombo@users.noreply.github.com>
2024-12-31 13:44:39 +0000

    Update BossCharacter.h
    Updated Code

--- BossCharacter.h
+++ BossCharacter.h
@@ -5,18 +5,37 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include "Interfaces/EnemyInterface.h"
+#include "Characters/EEnemyState.h"
+#include "Interfaces/FighterInterface.h"
 #include "BossCharacter.generated.h"
 
 
 UCLASS()
-class HERO_SOULS_API ABossCharacter : public ACharacter, public IEnemyInterface 
+class HERO_SOULS_API ABossCharacter : public ACharacter, public IEnemyInterface, public IFighterInterface
 {
 	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere)
+	TEnumAsByte<EEnemyState> InitialState;
+
+	class UBlackboardComponent* BlackboardComp;
+
+	UPROPERTY(EditAnywhere)
+	UAnimMontage* DeathAnim;
+
+	class AAIController* ControllerRef;
+
 
 public:
 	// Sets default values for this character's properties
 	ABossCharacter();
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	class UStatsComponent* StatsComp;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    class UCombatComponent* CombatComp;
+		
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -28,4 +47,23 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	UFUNCTION(BlueprintCallable)
+	void DetectPawn(APawn* DetectedPawn, APawn* PawnToDetect);
+
+	virtual float GetDamage() override;
+
+	virtual void Attack() override;
+
+	virtual float GetAnimDuration() override;
+
+	virtual float GetMeleeRange() override;
+
+	UFUNCTION()
+	void HandlePlayerDeath();
+
+	UFUNCTION(BlueprintCallable)
+	void HandleDeath();
+
+	UFUNCTION()
+	void FinishDeathAnim();
 };
