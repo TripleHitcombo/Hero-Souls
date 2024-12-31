4fa5ac93d574507954d510d175787960a33ae904
Carl Jones <161535663+TripleHitcombo@users.noreply.github.com>
2024-12-31 13:33:46 +0000

    Added new classes

--- BTT_RangeAttack.cpp
+++ BTT_RangeAttack.cpp
@@ -4,6 +4,10 @@
 #include "Characters/BTT_RangeAttack.h"
 #include "AIController.h"
 #include "GameFramework/Character.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Characters/EEnemyState.h"
+#include"Interfaces/FighterInterface.h"
 
 EBTNodeResult::Type UBTT_RangeAttack::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
@@ -15,9 +19,43 @@ EBTNodeResult::Type UBTT_RangeAttack::ExecuteTask(UBehaviorTreeComponent& OwnerC
 	if (!IsValid(CharacterRef))
 	{
 		return EBTNodeResult::Failed;
+	}
+
+	float Distance{
+		OwnerComp.GetBlackboardComponent()
+			->GetValueAsFloat(TEXT("Distance"))
 	};
 
+	IFighterInterface* FighterRef
+	{
+		Cast<IFighterInterface>(CharacterRef)
+	};
+
+	if (Distance < FighterRef->GetMeleeRange())
+	{
+		OwnerComp.GetBlackboardComponent()
+			->SetValueAsEnum(TEXT("CurrentState"), EEnemyState::Melee);
+
+		AbortTask(OwnerComp, NodeMemory);
+
+		return EBTNodeResult::Aborted;
+	}
+
+
 	CharacterRef->PlayAnimMontage(AnimMontage);
+
+	double RandomValue{ UKismetMathLibrary::RandomFloat()};
+
+	if (RandomValue > Threshold)
+	{
+		Threshold = 0.9;
+		OwnerComp.GetBlackboardComponent()
+			->SetValueAsEnum(TEXT("CurrentState"), EEnemyState::Charge);
+	}
+	else
+	{
+		Threshold -= 0.1;
+	}
 		
 	return EBTNodeResult::Succeeded;
 
