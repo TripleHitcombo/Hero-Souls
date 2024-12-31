22c2b8a664b828eb140c646076bcd912a1906e12
Carl Jones <161535663+TripleHitcombo@users.noreply.github.com>
2024-12-31 13:41:38 +0000

    Update TraceComponent.cpp
    Updated Code

--- TraceComponent.cpp
+++ TraceComponent.cpp
@@ -1,137 +1,142 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Combat/TraceComponent.h"
 #include "Kismet/KismetSystemLibrary.h"
 #include "Kismet/KismetMathLibrary.h"
 #include "Engine/DamageEvents.h"
 #include "Interfaces/FighterInterface.h"
+#include "Kismet/GameplayStatics.h"
 
 // Sets default values for this component's properties
 UTraceComponent::UTraceComponent()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
+    // Set this component to be initialized when the game starts, and to be ticked every frame. 
+    PrimaryComponentTick.bCanEverTick = true;
 }
-
 
 // Called when the game starts
 void UTraceComponent::BeginPlay()
 {
-	Super::BeginPlay();
+    Super::BeginPlay();
 
-	SkeletalComp = GetOwner()
-		->FindComponentByClass<USkeletalMeshComponent>();
+    SkeletalComp = GetOwner()->FindComponentByClass<USkeletalMeshComponent>();
+    if (!SkeletalComp)
+    {
+        UE_LOG(LogTemp, Error, TEXT("SkeletalComp is null in TraceComponent::BeginPlay"));
+    }
 }
-
 
 // Called every frame
 void UTraceComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	if (!bIsAttacking)
-	{
-		return;
-	}
+    if (!bIsAttacking)
+    {
+        return;
+    }
 
-	FVector StartSocketLocation{ SkeletalComp->GetSocketLocation(Start) };
-	FVector EndSocketLocation{ SkeletalComp->GetSocketLocation(End) };
-	FQuat ShapeRotation{ SkeletalComp->GetSocketQuaternion(Rotation) };
+    TArray<FHitResult> AllResults;
 
-	TArray<FHitResult> OutResults;
-	double WeapoDistance{
-		FVector::Distance(StartSocketLocation, EndSocketLocation)
-	};
-	FVector BoxHalfExtent{
-		BoxCollisionLength, BoxCollisionLength, WeapoDistance
-	};
-	BoxHalfExtent /= 2; // BoxHalfExtent = BoxHalfExtent / 2;
-	FCollisionShape Box{
-		FCollisionShape::MakeBox(BoxHalfExtent)
-	};
-	FCollisionQueryParams IgnoreParams{
-		FName { TEXT("Ignore Params") },
-		false,
-		GetOwner()
-	};
+    for (const FTraceSockets& Socket : Sockets)
+    {
+        FVector StartSocketLocation = SkeletalComp->GetSocketLocation(Socket.Start);
+        FVector EndSocketLocation = SkeletalComp->GetSocketLocation(Socket.End);
+        FQuat ShapeRotation = SkeletalComp->GetSocketQuaternion(Socket.Rotation);
 
-	bool bHasFoundTargets{ GetWorld()->SweepMultiByChannel(
-		OutResults,
-		StartSocketLocation,
-		EndSocketLocation,
-		ShapeRotation,
-		ECollisionChannel::ECC_GameTraceChannel1,
-		Box,
-		IgnoreParams
-	) };
+        TArray<FHitResult> OutResults;
+        double WeaponDistance = FVector::Distance(StartSocketLocation, EndSocketLocation);
+        FVector BoxHalfExtent = FVector(BoxCollisionLength, BoxCollisionLength, WeaponDistance) / 2.0f;
+        FCollisionShape Box = FCollisionShape::MakeBox(BoxHalfExtent);
+        FCollisionQueryParams IgnoreParams(FName(TEXT("IgnoreParams")), false, GetOwner());
 
-	if (bDebugMode)
-	{
-		FVector CentrePoint //Calculate centre point of box using a linear interpolation
-		{
-			UKismetMathLibrary::VLerp
-			(
-				StartSocketLocation,
-				EndSocketLocation, 0.5f
-			)
-		};
-		
-		UKismetSystemLibrary::DrawDebugBox
-		(
-			GetWorld(),
-			CentrePoint,
-			Box.GetExtent(),
-			bHasFoundTargets ? FLinearColor::Green : FLinearColor::Red, //Ternary Operator to select value either red or green
-			ShapeRotation.Rotator(),
-			1.0f,
-			2.0f
-		);
-	}
+        bool bHasFoundTargets = GetWorld()->SweepMultiByChannel(
+            OutResults,
+            StartSocketLocation,
+            EndSocketLocation,
+            ShapeRotation,
+            ECollisionChannel::ECC_GameTraceChannel1,
+            Box,
+            IgnoreParams
+        );
 
-	if (OutResults.Num() == 0) 
-	{
-		return;
-	}
+        AllResults.Append(OutResults);
 
-	float CharacterDamage{ 0.0f };
+        if (bDebugMode)
+        {
+            FVector CentrePoint = UKismetMathLibrary::VLerp(StartSocketLocation, EndSocketLocation, 0.5f);
+            UKismetSystemLibrary::DrawDebugBox(
+                GetWorld(),
+                CentrePoint,
+                Box.GetExtent(),
+                bHasFoundTargets ? FLinearColor::Green : FLinearColor::Red,
+                ShapeRotation.Rotator(),
+                1.0f,
+                2.0f
+            );
+        }
+    }
 
-	IFighterInterface* FighterRef{ Cast<IFighterInterface>(GetOwner()) };
+    if (AllResults.Num() == 0)
+    {
+        UE_LOG(LogTemp, Warning, TEXT("No targets found during attack trace"));
+        return;
+    }
 
-	if (FighterRef)
-	{
-		CharacterDamage = FighterRef->GetDamage();
-	}
+    float CharacterDamage = 0.0f;
+    IFighterInterface* FighterRef = Cast<IFighterInterface>(GetOwner());
+    if (FighterRef)
+    {
+        CharacterDamage = FighterRef->GetDamage();
+    }
 
-	FDamageEvent TargetAttackedEvent; 
+    FDamageEvent TargetAttackedEvent;
 
-	for (const FHitResult& Hit: OutResults) //& asks for ref instead of new copy, much better performance
-	{
-		AActor* TargetActor { Hit.GetActor() };
+    for (const FHitResult& Hit : AllResults)
+    {
+        AActor* TargetActor = Hit.GetActor();
+        if (TargetsToIgnore.Contains(TargetActor))
+        {
+            continue;
+        }
 
-		if (TargetsToIgnore.Contains(TargetActor))
-		{
-			continue; //use contunue to keep going instead of return which stops the flow
-		}
+        TargetActor->TakeDamage(
+            CharacterDamage,
+            TargetAttackedEvent,
+            GetOwner()->GetInstigatorController(),
+            GetOwner()
+        );
 
-		TargetActor->TakeDamage
-		(
-			CharacterDamage,
-			TargetAttackedEvent,
-			GetOwner()->GetInstigatorController(),
-			GetOwner()
-		  
-		);
+        TargetsToIgnore.AddUnique(TargetActor);
 
-		TargetsToIgnore.AddUnique(TargetActor); //unique make sure value already exists and only adds new ones
-	}
+        UGameplayStatics::SpawnEmitterAtLocation
+        (
+            GetWorld(),
+            HitParticleTemplate,
+            Hit.ImpactPoint
+        );
+
+    }
 }
 
+// Start attack logic
+void UTraceComponent::BeginAttack()
+{
+    bIsAttacking = true;
+    TargetsToIgnore.Empty(); // Clear the ignored targets at the start of each attack
+    UE_LOG(LogTemp, Warning, TEXT("Attack started, bIsAttacking set to true"));
+}
+
+// End attack logic
+void UTraceComponent::EndAttack()
+{
+    bIsAttacking = false;
+    HandleResetAttack(); // Clear ignored targets
+    UE_LOG(LogTemp, Warning, TEXT("Attack ended, bIsAttacking set to false"));
+}
+
+// Reset targets to ignore
 void UTraceComponent::HandleResetAttack()
 {
-	TargetsToIgnore.Empty();
+    TargetsToIgnore.Empty();
+    UE_LOG(LogTemp, Warning, TEXT("Resetting TargetsToIgnore array"));
 }
 
