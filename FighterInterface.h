fb666a235da7d5774cb3007c58082fbde96fedf0
Carl Jones <161535663+TripleHitcombo@users.noreply.github.com>
2024-12-31 14:20:28 +0000

    Update FighterInterface.h
    Updated Code

--- FighterInterface.h
+++ FighterInterface.h
@@ -24,4 +24,12 @@ class HERO_SOULS_API IFighterInterface
 public:
 
 	virtual float GetDamage() { return 0.0f; }
+
+	virtual void Attack() {}
+
+	virtual float GetAnimDuration() { return 0.0f; }
+
+	virtual float GetMeleeRange() { return 0.0f; }
+
+	virtual bool CanTakeDamage(AActor* Oppenent) { return true; }
 };
